1a88d20f60d0e0f01a4e4037e84486a2504a389e
Luik <t.t.luik@amsterdamumc.nl>
2026-09-23 18:36:04 +0200

    chore: Prepare v0.6.1 release

--- src/biomero_zarr_viewer/__init__.py
+++ src/biomero_zarr_viewer/__init__.py
@@ -1,5 +1,5 @@
 """Authenticated OME-Zarr viewing for BIOMERO and OMERO.web."""
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 default_app_config = "biomero_zarr_viewer.apps.BiomeroZarrViewerConfig"
