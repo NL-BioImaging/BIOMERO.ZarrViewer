d8548cc68225b4ab62df1cc2de389ba36aa9133b
Luik <t.t.luik@amsterdamumc.nl>
2026-09-23 18:41:10 +0200

    build: derive package version from git tags

--- src/biomero_zarr_viewer/__init__.py
+++ src/biomero_zarr_viewer/__init__.py
@@ -1,5 +1,10 @@
 """Authenticated OME-Zarr viewing for BIOMERO and OMERO.web."""
 
-__version__ = "0.6.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("biomero-zarr-viewer")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
 
 default_app_config = "biomero_zarr_viewer.apps.BiomeroZarrViewerConfig"
